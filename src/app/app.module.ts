cab50ae4cf333bde2ec6b9e00d302e79912164a9
jaseel <jaseeljctcs@gmail.com>
2022-12-06 17:52:18 +0530

    view page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewCourseComponent } from './view-course/view-course.component';
 const myroute:Routes=[
   {
     path:"",
     component:AddComponent
+  },{
+    path:"view",
+    component:ViewCourseComponent
   }
+
+  
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    AddComponent
+    AddComponent,
+    ViewCourseComponent
   ],
   imports: [
     BrowserModule,
